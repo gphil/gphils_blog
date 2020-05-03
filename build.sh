26cfc1fd9f72b3cfbaa80ffb287202d849194931
Greg Phillips <gphil@gphil.net>
2020-05-03 14:40:40 -0400

    use correct stack dir name

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # build script for netlify builds
 wget https://get.haskellstack.org/stable/linux-x86_64.tar.gz
 tar -xvzf linux-x86_64.tar.gz
-cp linux-x86_64/stack ./stack
+cp stack*/stack ./stack
 ./stack build
 ./stack exec site build
