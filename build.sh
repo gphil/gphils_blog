91e2884f882ccdbdb6673bb74d12b4c346397358
Greg Phillips <gphil@gphil.net>
2020-05-18 20:38:11 -0400

    try printing out some env info

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 # build script for netlify builds
 
-BUILD_CACHE_DIR=/opt/build/cache
+BUILD_CACHE_DIR=/opt/buildhome/cache
 
 if [ ! -f $BUILD_CACHE_DIR/stack ]; then
     wget https://get.haskellstack.org/stable/linux-x86_64.tar.gz
     tar -xvzf linux-x86_64.tar.gz
+    echo $NETLIFY_CACHE_DIR
+    pwd
+    ls
     cp stack*/stack $BUILD_CACHE_DIR/stack
+    cp .stack $BUILD_CACHE_DIR/.stack
 fi
 
 $BUILD_CACHE_DIR/stack build --fast
