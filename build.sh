ee9811a9e25a8be88d4356fd6d83c83dcd3b9953
Greg Phillips <gphil@gphil.net>
2020-05-18 20:40:58 -0400

    what's in opt?

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 # build script for netlify builds
 
+find /opt/.
+
 BUILD_CACHE_DIR=/opt/buildhome/cache
 
 if [ ! -f $BUILD_CACHE_DIR/stack ]; then
     wget https://get.haskellstack.org/stable/linux-x86_64.tar.gz
     tar -xvzf linux-x86_64.tar.gz
-    echo $NETLIFY_CACHE_DIR
-    pwd
-    ls
     cp stack*/stack $BUILD_CACHE_DIR/stack
     cp .stack $BUILD_CACHE_DIR/.stack
 fi
