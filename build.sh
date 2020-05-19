80c19b702e2add149457786059f5c3a9cbc554af
Greg Phillips <gphil@gphil.net>
2020-05-18 20:25:35 -0400

    try specifying .cache for build cache

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 # build script for netlify builds
 
-if [ ! -f $NETLIFY_CACHE_DIR/stack ]; then
+BUILD_CACHE_DIR=.cache
+
+if [ ! -f $BUILD_CACHE_DIR/stack ]; then
     wget https://get.haskellstack.org/stable/linux-x86_64.tar.gz
     tar -xvzf linux-x86_64.tar.gz
-    cp stack*/stack $NETLIFY_CACHE_DIR/stack
+    cp stack*/stack $BUILD_CACHE_DIR/stack
 fi
 
-$NETLIFY_CACHE_DIR/stack build --fast
-$NETLIFY_CACHE_DIR/stack exec site build
+$BUILD_CACHE_DIR/stack build --fast
+$BUILD_CACHE_DIR/stack exec site build
