ff9b319df10b991569eb148d63e62965d22bc9fd
Greg Phillips <gphil@gphil.net>
2020-05-18 20:49:47 -0400

    add some logging

--- build.sh
+++ build.sh
@@ -6,8 +6,13 @@ if [ ! -f $BUILD_CACHE_DIR/stack ]; then
     wget https://get.haskellstack.org/stable/linux-x86_64.tar.gz
     tar -xvzf linux-x86_64.tar.gz
     cp stack*/stack $BUILD_CACHE_DIR/stack
+    echo "Current dir:\n"
+    pwd
+    echo "Build cache dir:\n"
+    echo $BUILD_CACHE_DIR
+    $BUILD_CACHE_DIR/stack build --fast
     cp .stack $BUILD_CACHE_DIR/.stack
 fi
 
-$BUILD_CACHE_DIR/stack build --fast
 $BUILD_CACHE_DIR/stack exec site build
+
