b22f07beca35ab48b4d9a4656d439a4f31a83583
Greg Phillips <gphil@gphil.net>
2020-05-18 20:31:09 -0400

    try /opt/build/cache

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # build script for netlify builds
 
-BUILD_CACHE_DIR=/opt/buildhome/cache
+BUILD_CACHE_DIR=/opt/build/cache
 
 if [ ! -f $BUILD_CACHE_DIR/stack ]; then
     wget https://get.haskellstack.org/stable/linux-x86_64.tar.gz
